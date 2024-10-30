ca0213b330a142d87591f534df3ab33ecfcd0ba1
nadira1187 <nadira35-1187@s.diu.edu.bd>
2024-10-30 20:40:55 +0600

    added the hero section of my portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+ import { ThemeProvider } from "./provider";
+ if (typeof window !== "undefined") {
+  localStorage.removeItem("theme"); // Clear existing theme preferences
+}
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Nadira's Portfolio",
+  description: "Minimal portfolio by Nadira",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark" suppressHydrationWarning>
+     <head />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+       <FloatingNav  navItems = {[
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+]}/>
+        <Hero></Hero>
+        <Grid></Grid>
+      </div>
+    </main>
+  );
+}
